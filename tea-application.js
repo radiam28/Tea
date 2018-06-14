60d04511c397d3a6bba04db7c86878bd5103962f
radiam28 <34085658+radiam28@users.noreply.github.com>
2018-06-13 23:47:32 -0500

    Add files via upload

---
+++ tea-application.js
@@ -0,0 +1,134 @@
+var WhiteTea = {
+    Water: {
+        description: '175-185 degrees Farenheit',
+        value: '180'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '3'
+    },
+    Time: {
+        description: '1-3 minutes',
+        value: '2'
+    },
+};
+
+var GreenTea = {
+    Water: {
+        description: '180-185 degrees Farenheit',
+        value: '182.5'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '1'
+    },
+    Time: {
+        description: '3 minutes',
+        value: '3'
+    },
+};
+
+var BlackTea = {
+    Water: {
+        description: '206 degrees Farenheit',
+        value: '206'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '1'
+    },
+    Time: {
+        description: '3-5 minutes',
+        value: '4'
+    },
+};
+
+var DarjeelingTea = {
+    Water: {
+        description: '185 degrees Farenheit',
+        value: '185'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '1'
+    },
+    Time: {
+        description: '3 minutes',
+        value: '3'
+    },
+};
+
+var OolongRolledTea = {
+    Water: {
+        description: '185-206 degrees Farenheit',
+        value: '195.5'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '1'
+    },
+    Time: {
+        description: '3-5 minutes',
+        value: '4'
+    },
+};
+
+var OolongLongLeafTea = {
+    Water: {
+        description: '185-206 degrees Farenheit',
+        value: '195.5'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '3'
+    },
+    Time: {
+        description: '3-5 minutes',
+        value: '4'
+    },
+};
+
+var TisaneTea = {
+    Water: {
+        description: '206 degrees Farenheit',
+        value: '206'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '3'
+    },
+    Time: {
+        description: '5-7 minutes',
+        value: '6'
+    },
+};
+
+var RawPuerhTea = {
+    Water: {
+        description: '195 degrees Farenheit',
+        value: '195'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '3'
+    },
+    Time: {
+        description: '3-5 minutes',
+        value: '4'
+    },
+};
+
+var HalfandFullBakedPuerhTea = {
+    Water: {
+        description: '206 degrees Farenheit',
+        value: '206'
+    },
+    Teaamount: {
+        description: '(6-8 Oz Water)',
+        value: '3'
+    },
+    Time: {
+        description: '3-5 minutes',
+        value: '4'
+    },
+};
